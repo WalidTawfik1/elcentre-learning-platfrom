8f9d14d71148f93a6b28caf2cc8272b2be1cc265
Walid Tawfik <walidtawfik362@gmail.com>
2025-04-06 11:58:30 +0200

    Initial Commit

---
+++ ElCentre.Infrastructure/Data/ElCentreDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ElCentre.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElCentre.Infrastructure.Data
+{
+    public class ElCentreDbContext:IdentityDbContext<AppUser>
+    {
+        public ElCentreDbContext(DbContextOptions<ElCentreDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+    
+    
+}
---
+++ ElCentre.Infrastructure/InfrastrucutreRegistration.cs
@@ -0,0 +1,41 @@
+﻿using ElCentre.Core.Entities;
+using ElCentre.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElCentre.Infrastructure
+{
+    public static class InfrastrucutreRegistration
+    {
+        public static IServiceCollection InfrastructureConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ElCentreDbContext>((options) =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ElCentreDatabase"));
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            }).AddEntityFrameworkStores<ElCentreDbContext>()
+              .AddDefaultTokenProviders();
+
+            return services;
+        }
+        
+    }
+    
+}
